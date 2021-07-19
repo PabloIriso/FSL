4e2c4e59b84ce020a89b599326ee4dbf93593cb7
PabloIriso <79107558+PabloIriso@users.noreply.github.com>
2021-07-19 13:54:08 +0400

    Create probtrackx_putamen.sh

---
+++ probtrackx_putamen.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+DIR="/media/estela/HD_2/Pablo/Analysis"
+
+for group in Controls Patients; do
+    echo $group
+    for folder in $DIR/$group/*; do
+        name=$(basename "$folder")  
+        echo $name 
+        for side in left right; do
+            for cortical in caudate accumbens putamen; do # Executive Motivational Motor; do
+                for exclusion in Exclusion No_exclusion; do
+
+                    mkdir "$DIR/Tractography_${group}/$name" #folder that will contain probtrackx output
+                    mkdir "$DIR/Tractography_${group}/$name/$exclusion"
+                    mkdir "$DIR/Tractography_${group}/$name/$exclusion/putamen"
+                    mkdir "$DIR/Tractography_${group}/$name/$exclusion/putamen/$side"  #folder that will contain probtrackx output of each side of the brain
+
+                    bedpost_dir="$DIR/Bedpost_${group}/$name/dmri.bedpostX" #directory where the bedpostX output is stored
+
+                    if [[ "$exclusion" == "Exclusion" ]] # run the analysis adding the two cortical masks not used as seed as exclusion masks
+
+                    then 
+
+# -x,--seed     Seed volume or list (ascii text file) of volumes and/or surfaces - cortical mask of interest
+# -s,--samples	Basename for samples files - e.g. 'merged'
+# -m,--mask	Bet binary mask file in diffusion space
+
+# --dir		Directory to put the final volumes in - code makes this directory - default='logdir'
+# --stop		Stop tracking at locations given by this mask file
+# --avoid		Reject pathways passing through locations given by this mask
+
+                        echo "Total exclusion probtrakx"
+                        /usr/share/fsl/5.0/bin/probtrackx2 -x $DIR/$group/$name/Cortical_masks/putamen/FA_image_${side}_normalized.nii.gz -l --onewaycondition -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --avoid="$DIR/$group/$name/Exclusion_masks/exclusion_mask_total_putamen_${side}.nii.gz" --forcedir --opd -s $bedpost_dir/merged -m $bedpost_dir/nodif_brain_mask --dir=$DIR/Tractography_${group}/$name/$exclusion/putamen/$side/seed1 --stop=$DIR/$group/$name/putamen_masks/FA_transformation/smoothed_${side}_putamen.nii.gz 
+
+
+                        /usr/share/fsl/5.0/bin/probtrackx2 -x $DIR/$group/$name/putamen_masks/FA_transformation/smoothed_${side}_putamen.nii.gz -l --onewaycondition -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --avoid="$DIR/$group/$name/Exclusion_masks/exclusion_mask_total_putamen_${side}.nii.gz" --forcedir --opd -s $bedpost_dir/merged -m $bedpost_dir/nodif_brain_mask --dir=$DIR/Tractography_${group}/$name/$exclusion/putamen/$side/seed2 --waypoints=$DIR/$group/$name/Cortical_masks/putamen/FA_image_${side}_normalized.nii.gz --waycond=AND
+
+                    else # run the analysis without adding the two cortical masks not used as seed as exclusion masks
+                        echo "CSF_midline exclusion probtrakx"
+                        /usr/share/fsl/5.0/bin/probtrackx2 -x $DIR/$group/$name/Cortical_masks/putamen/FA_image_${side}_normalized.nii.gz -l --onewaycondition -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --avoid="$DIR/$group/$name/Exclusion_masks/exclusion_mask_CSF_middline_putamen_${side}.nii.gz" --forcedir --opd -s $bedpost_dir/merged -m $bedpost_dir/nodif_brain_mask --dir=$DIR/Tractography_${group}/$name/$exclusion/putamen/$side/seed1 --stop=$DIR/$group/$name/putamen_masks/FA_transformation/smoothed_${side}_putamen.nii.gz 
+
+
+                        /usr/share/fsl/5.0/bin/probtrackx2 -x $DIR/$group/$name/putamen_masks/FA_transformation/smoothed_${side}_putamen.nii.gz -l --onewaycondition -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --avoid="$DIR/$group/$name/Exclusion_masks/exclusion_mask_CSF_middline_putamen_${side}.nii.gz" --forcedir --opd -s $bedpost_dir/merged -m $bedpost_dir/nodif_brain_mask --dir=$DIR/Tractography_${group}/$name/$exclusion/putamen/$side/seed2 --waypoints=$DIR/$group/$name/Cortical_masks/putamen/FA_image_${side}_normalized.nii.gz --waycond=AND
+
+                                                
+
+                    fi
+
+                    done;
+            done;
+        done;
+    done; 
+done;
+
+
+# Los cuatro parrafos se corresponden con Exclusion-No_exclusion y seed1-seed2. Hemos cambiado la generacion de carpetas (eliminado subcarpeta $cortical). 
+# Dado que queremos establecer la conexion con la region correspondiente de la corteza, eliminamos iteracion cortical (Cortical_masks/putamen)
+# Tambien cambiamos directorio de salida. 
+# Cambiamos tambien el directorio que se corresponde con exclusion_masks y CSF Middlines. En este caso, la unica exclusion y csf middline mask sera putamen.
+
